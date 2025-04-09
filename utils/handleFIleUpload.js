dbc039b2014fdd0e0266b0a077b89ebd21ca3ec0
Umer Farooq <umerfarooq2821035@gmail.com>
2025-04-09 18:01:51 +0500

    filehandling logic done

--- utils/handleFIleUpload.js
+++ utils/handleFIleUpload.js
@@ -1,1 +1,21 @@
 import multer from 'multer'
+
+let allowedFiles = ['image/jpeg','image/webp','image/png']
+
+const storage = multer.memoryStorage();
+
+const fileFilterFn = (req,file,cb) => {
+  if(allowedFiles.includes(file.mimetype)){
+    cb(null,true)
+  }else{
+    cb(new Error('Only JPEG , PNG and WEBP files are allowed') , false)
+  }
+}
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilterFn,
+  limits: {fileSize: 1024 * 1024 * 10}
+})
+
+export default upload;
