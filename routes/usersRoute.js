7903729311dcddbc9bf2fd7fbeb5d89ae3c3e97e
Umer Farooq <umerfarooq2821035@gmail.com>
2025-04-09 17:18:04 +0500

    Adding nodemon to package.json file

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import {fetchAllUsers,addUser,loginUser,fetchUserByID} from '../controller/userController.js'
-import upload from '../utils/handleFIleUpload.js';
+import fileUploadMiddleware from '../cloudnary/uploadImage.js'
 
 const userRoute = express.Router();
 
 userRoute.get('/', fetchAllUsers)
 userRoute.get('/:id' ,fetchUserByID)
-userRoute.post('/addUser',addUser)
+userRoute.post('/addUser',fileUploadMiddleware("profile"),addUser)
 userRoute.post('/loginUser' , loginUser)
 
 export default userRoute;
